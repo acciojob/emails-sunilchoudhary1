2a25594f6be55bfdab8778c838be85f8a36b5516
Sunil <sunilkumar010@gmail.com>
2023-02-18 20:01:04 +0530

    Solution 1

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,33 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(this.password.equals(oldPassword))
+        {
+            if(newPassword.length()>=8)
+            {
+                int up=0;
+                int lo=0;
+                int digit=0;
+                int sp=0;
+                for(int i =0 ; i <newPassword.length();i++)
+                {
+                    char ch =newPassword.charAt(i);
+                    if(ch >='A' && ch <='Z')
+                        up++;
+                    else if(ch>='a' && ch<='z')
+                        lo++;
+                    else if(ch>='0' && ch <='9')
+                        digit++;
+                    else
+                        sp++;
+                }
+                if(up >=1 && lo>=1 && digit >=1 && sp>=1)
+                {
+                    this.password=newPassword;
+                }
+
+            }
+
+        }
     }
 }
